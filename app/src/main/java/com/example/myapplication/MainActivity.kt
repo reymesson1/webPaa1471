7e27feb6ea895ad0540a8b6646d09b56ca8066bf
Rey Messon <rey_messon@hotmail.com>
2022-03-19 10:37:13 -0400

    Adding

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,22 +16,63 @@ import org.jetbrains.anko.doAsync
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.reflect.Array
 
 class MainActivity : AppCompatActivity() {
 
     var restAPI = RestAPI()
 
-    val datos = arrayOf("1", "2", "3", "4",
-        "5", "6", "7", "8",
-        "9", "9", "10", "12",
-        "13", "14", "15")
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var datos = emptyArray<String>();
+        var datos2 = emptyArray<String>();
+        var datos3 = emptyArray<String>();
+        var datos4 = emptyArray<String>();
+        var datosx = emptyArray<String>();
 
+        for (i in 1..9) {
+            datos+=i.toString()
+            Log.i("response", i.toString())
+        }
+        for (i in 1..99) {
+            if(i<10){
+                datos2+="0"+i.toString()
+                Log.i("response", i.toString())
+            }else{
+                datos2+=i.toString()
+                Log.i("response", i.toString())
 
+            }
+        }
+        for (i in 1..999) {
+            if(i<10){
+                datos3+="00"+i.toString()
+                Log.i("response", i.toString())
+            }else if(i<100){
+                datos3+="0"+i.toString()
+                Log.i("response", i.toString())
+            }else{
+                datos3+=i.toString()
+                Log.i("response", i.toString())
+            }
+        }
+        for (i in 1..9999) {
+            if(i<10){
+                datos4+="000"+i.toString()
+                Log.i("response", i.toString())
+            }else if(i<100){
+                datos4+="00"+i.toString()
+                Log.i("response", i.toString())
+            }else if(i<1000){
+                datos4+="0"+i.toString()
+                Log.i("response", i.toString())
+            }else{
+                datos4+=i.toString()
+                Log.i("response", i.toString())
+            }
+        }
 
 
         doAsync {
@@ -40,24 +81,75 @@ class MainActivity : AppCompatActivity() {
 
                 getData()
 
-                datos.forEach { at->
-
-                    Log.i("response", "primero")
-                }
-
-                Thread.sleep(2000)
-
-                datos.forEach { at->
-
-                    Log.i("response", at)
-                }
-
                 val adaptador = ArrayAdapter(this@MainActivity,
                     R.layout.elemento_de_lista,
                     datos)
                 miLista.adapter = adaptador
 
                 miLista.onItemClickListener =
+                    object : AdapterView.OnItemClickListener{
+                        override fun onItemClick(
+                            parent: AdapterView<*>?,
+                            view: View?,
+                            position: Int,
+                            id: Long
+                        ) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Escogido: ${miLista.getItemAtPosition(position)}",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+
+                val adaptador2 = ArrayAdapter(this@MainActivity,
+                    R.layout.elemento_de_lista,
+                    datos2)
+                miLista2.adapter = adaptador2
+
+                miLista2.onItemClickListener =
+                    object : AdapterView.OnItemClickListener{
+                        override fun onItemClick(
+                            parent: AdapterView<*>?,
+                            view: View?,
+                            position: Int,
+                            id: Long
+                        ) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Escogido: ${miLista.getItemAtPosition(position)}",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+
+                val adaptador3 = ArrayAdapter(this@MainActivity,
+                    R.layout.elemento_de_lista,
+                    datos3)
+                miLista3.adapter = adaptador3
+
+                miLista3.onItemClickListener =
+                    object : AdapterView.OnItemClickListener{
+                        override fun onItemClick(
+                            parent: AdapterView<*>?,
+                            view: View?,
+                            position: Int,
+                            id: Long
+                        ) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Escogido: ${miLista.getItemAtPosition(position)}",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+
+                val adaptador4 = ArrayAdapter(this@MainActivity,
+                    R.layout.elemento_de_lista,
+                    datos4)
+                miLista4.adapter = adaptador4
+
+                miLista4.onItemClickListener =
                     object : AdapterView.OnItemClickListener{
                         override fun onItemClick(
                             parent: AdapterView<*>?,
@@ -91,20 +183,6 @@ class MainActivity : AppCompatActivity() {
                 premio.setText(response.body()!!.precio.toString())
                 debo.setText(response.body()!!.debo.toString())
                 cupo.setText(response.body()!!.cupo.toString())
-
-                response.body()!!.jugados.forEachIndexed { index, at ->
-
-//                    datos.set(index)
-//                    datos.fill(index.toString())
-                    datos.toMutableList().add(index.toString())
-
-                    datos.forEach {
-
-                        Log.i("responseNum",it.toString())
-                    }
-
-                }
-
 
 //                response.body()!!.jugados.forEachIndexed  {  index,at->
 //
---
+++ app/src/main/java/com/example/myapplication/MiddleActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.myapplication.Model.ResponseRPEstado
+import com.example.myapplication.Model.RestAPI
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_middle.*
+import org.jetbrains.anko.activityUiThread
+import org.jetbrains.anko.doAsync
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MiddleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_middle)
+
+        btn_next.setOnClickListener {
+
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+
+    }
+
+}
